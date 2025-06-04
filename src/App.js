81a75a06f25169223bf96252ff372174b291b86f
Kishouu <diachuksviatoslav@gmail.com>
2025-06-04 13:59:59 +0200

    Setup server

--- src/App.js
+++ src/App.js
@@ -2,21 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  const[items,setItems] = useState([]);
+
+	useEffect(() => {
+		fetch("http://localhost:5000/api/items")
+		.then((responce) => responce.json())
+		.then((data) => setItems(data));
+	},[]);
+	return(
+		<div className = "App">
+		<header className="App-header">
+		<h1>Items</h1>
+		<ul>
+		{items.map((item) => (
+			<li key={item.id}>{item.name}</li>
+		))}
+		</ul>
       </header>
     </div>
   );
