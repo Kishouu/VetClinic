85e33f20d64a8afc46e1656f30b2244fba163996
Stepan <sturani@edu.cdv.pl>
2025-06-23 18:49:44 +0200

    Login/Register pages

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,30 +1,44 @@
 import React, { useState } from 'react';
 import LoginForm from '../components/LoginForm';
 import RegisterForm from '../components/RegisterForm';
+import '../components/UI/SignInPage.css';
 
 export default function SignInPage({ onLogin }) {
   const [showRegister, setShowRegister] = useState(false);
 
   return (
-    <div style={{ maxWidth: 400, margin: 'auto' }}>
-      {showRegister ? (
-        <>
-          <RegisterForm onRegister={onLogin} />
-          <p>
-            Already have an account?{' '}
-            <button onClick={() => setShowRegister(false)}>Login here</button>
-          </p>
-        </>
-      ) : (
-        <>
-          <LoginForm onLogin={onLogin} />
-          <p>
-            Don't have an account?{' '}
-            <button onClick={() => setShowRegister(true)}>Create account</button>
-          </p>
-        </>
-      )}
+    <div className="signin-background">
+      <div className="signin-container">
+        <div className="signin-card">
+          {showRegister ? (
+            <>
+              <RegisterForm onRegister={onLogin} />
+              <p className="signin-toggle-text">
+                <button
+                  id="toggle-login"
+                  className="toggle-button"
+                  onClick={() => setShowRegister(false)}
+                >
+                  Already have an account? Login.
+                </button>
+              </p>
+            </>
+          ) : (
+            <>
+              <LoginForm onLogin={onLogin} />
+              <p className="signin-toggle-text">
+                <button
+                  id="toggle-register"
+                  className="toggle-button"
+                  onClick={() => setShowRegister(true)}
+                >
+                  Don't have an account? Create it!
+                </button>
+              </p>
+            </>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
-
