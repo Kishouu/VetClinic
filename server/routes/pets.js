a8cd88f22bf273c419bfc82233c416b1a7e6269e
Stepan <sturani@edu.cdv.pl>
2025-06-24 02:57:12 +0200

    DashBoardCreated

--- server/routes/pets.js
+++ server/routes/pets.js
@@ -77,24 +77,23 @@ router.get('/:id', authenticateToken, async (req, res) => {
   }
 });
 
-// UPDATE pet
-router.put('/:id', authenticateToken, async (req, res) => {
+router.put('/:id', authenticateToken, requireDoctorOrAdmin, async (req, res) => {
   const petId = parseInt(req.params.id);
   const userId = req.user.id;
-  const { name, species } = req.body;
+  const { name, species, breed } = req.body;
 
   try {
     const pet = await prisma.pet.findUnique({
       where: { id: petId },
     });
 
-    if (!pet || pet.userId !== userId) {
-      return res.status(403).json({ error: 'Unauthorized or pet not found' });
+    if (!pet) {
+      return res.status(404).json({ error: 'Pet not found' });
     }
 
     const updatedPet = await prisma.pet.update({
       where: { id: petId },
-      data: { name, species },
+      data: { name, species, breed },
     });
 
     res.json({ message: 'Pet updated', pet: updatedPet });
@@ -103,8 +102,9 @@ router.put('/:id', authenticateToken, async (req, res) => {
   }
 });
 
+
 // DELETE pet
-router.delete('/:id', authenticateToken, async (req, res) => {
+router.delete('/:id', authenticateToken,requireDoctorOrAdmin, async (req, res) => {
   const petId = parseInt(req.params.id);
   const userId = req.user.id;
 
@@ -112,10 +112,6 @@ router.delete('/:id', authenticateToken, async (req, res) => {
     const pet = await prisma.pet.findUnique({
       where: { id: petId },
     });
-
-    if (!pet || pet.userId !== userId) {
-      return res.status(403).json({ error: 'Unauthorized or pet not found' });
-    }
 
     await prisma.pet.delete({ where: { id: petId } });
     res.json({ message: 'Pet deleted' });
